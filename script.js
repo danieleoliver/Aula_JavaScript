ff3cb7eaf854f48f277e4738df52d99b6ca5dcb4
danieleoliver <danieleoliver.sp@gmail.com>
2019-03-12 20:42:34 -0300

    Aula_Javascript

---
+++ script.js
@@ -0,0 +1,35 @@
+alert("Aula de JavaScript");
+var nome ="Daniele"; //variável tipo string//
+var idade = 27;
+
+//alert("Seu nome é " + nome + "sua idade é " + idade);
+
+/*function name(params) {
+
+}*/
+
+function soma() {
+    var n1 = 10;
+    var n2 = 15;
+    resultado = n1 + n2;
+    alert ("o resultado é " + resultado);
+}
+soma();
+
+function mult(v1,v2) {
+    var result = v1 * v2;
+    alert(result);
+}
+var x1 = 2;
+var x2 = 5;
+mult(x1,x2);
+
+
+//teste//
+
+//comentário de 1 linha
+/*várias
+linhas
+linha1
+linha2
+*/
